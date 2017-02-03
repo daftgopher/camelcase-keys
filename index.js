2d14e72f4825a99077eee5daec8275210775e12e
daftgopher <mattwaldron@gmail.com>
2017-02-03 11:03:00 -0500

    Convert to ES5

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 'use strict';
-const mapObj = require('map-obj');
-const camelCase = require('camelcase');
 
-const has = (arr, key) => arr.some(x => typeof x === 'string' ? x === key : x.test(key));
+var mapObj = require('map-obj');
+var camelCase = require('camelcase');
 
-module.exports = (input, opts) => {
+var has = function has(arr, key) {
+	return arr.some(function (x) {
+		return typeof x === 'string' ? x === key : x.test(key);
+	});
+};
+
+module.exports = function (input, opts) {
 	opts = Object.assign({
 		exclude: [],
 		deep: false
 	}, opts);
 
-	return mapObj(input, (key, val) => {
+	return mapObj(input, function (key, val) {
 		key = has(opts.exclude, key) ? key : camelCase(key);
 		return [key, val];
-	}, {deep: opts.deep});
+	}, { deep: opts.deep });
 };
